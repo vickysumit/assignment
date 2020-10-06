c34bfee05940d1e6d41d10fa7ca0bdb3ec221348
sumit <sharmasumit0308@gmail.com>
2020-10-06 09:03:01 +0530

    Updates

---
+++ precache-manifest.98e11c2601e74e3d340edb620dea93f6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62cef89e59784d1df9fe404ca6b2e2bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d11a08fc5834a06490c8",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bba656f134db518a1ae6",
+    "url": "/static/css/main.8f5151aa.chunk.css"
+  },
+  {
+    "revision": "d11a08fc5834a06490c8",
+    "url": "/static/js/2.23952b9e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.23952b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bba656f134db518a1ae6",
+    "url": "/static/js/main.251c1ab2.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "94799bf193eaa8fa30ab37bf9557e7ae",
+    "url": "/static/media/Logo2.94799bf1.jpg"
+  },
+  {
+    "revision": "96480b100c51ad8c5bd7bd9b0f92d38e",
+    "url": "/static/media/admin.96480b10.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a02885ef473dc753197d79930ab2c4c1",
+    "url": "/static/media/m1.a02885ef.jpg"
+  },
+  {
+    "revision": "67184173b0af641a747b78eeb5772372",
+    "url": "/static/media/pizza.67184173.jpg"
+  }
+]);
